30b43a8921a9dc046bd03663dd072ea2a8660d76
jcole001 <jcoleharry84@gmail.com>
2024-05-02 09:48:56 +0300

    remove unexisting file

---
+++ HBT/index.js
@@ -0,0 +1,24 @@
+// let firstcard = 3 ; 
+// let secondcard= 45 ; 
+
+// let sum = firstcard + secondcard
+
+// if (sum<=21){
+//     console.log("Play the game again")
+
+// }
+// else {
+//     console.log("You are out of the game")
+// }
+
+let age = document.getElementById("birthdate-el")
+
+console.log(age)
+
+// if(age<100){
+//     console.log("You still have a couple of years left")
+// } else if ( age === 100)
+// { console.log("Happy birthday I wish you a prosperous year")
+// }
+// else{console.log ("You've already gotten one. You bitch !!!! ")
+// } 
---
+++ welcome-application/index.js
@@ -0,0 +1,10 @@
+let welcomeEl= document.getElementById("welcome-el")
+
+let name="Julius"
+let greetings = "welcome back!!"
+
+
+welcomeEl.innerText = name +", " + greetings
+
+
+welcomeEl.innerText += "😂😂"
